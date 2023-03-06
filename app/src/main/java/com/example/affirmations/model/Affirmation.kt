6ed97bfc0e2a67b2ebb0b57031c8eb489c6a8bb6
hitak77 <hitak77@naver.com>
2023-03-06 18:37:04 +0900

    Affirmation 목록 항목에 이미지 추가 까지.

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,3 +1,10 @@
 package com.example.affirmations.model
 
-data class Affirmation(val stringResourceId: Int)
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+// 리소스 주석 달기위해서
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+    )
